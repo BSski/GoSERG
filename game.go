7c4e3885ed55bb41e74789a4ccb975ad8948da68
BSski <swedrowskibartosz@gmail.com>
2023-03-29 10:59:59 +0200

    add some UI lines and rectangles

--- game.go
+++ game.go
@@ -3,6 +3,7 @@ package main
 import (
 	"GoSERG/fonts"
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 	"github.com/hajimehoshi/ebiten/v2/text"
 	"golang.org/x/image/font"
 	"golang.org/x/image/font/opentype"
@@ -11,8 +12,9 @@ import (
 )
 
 var (
-	pressStart2P   font.Face
-	mPlus1pRegular font.Face
+	pressStart2P     font.Face
+	mPlus1pRegular11 font.Face
+	mPlus1pRegular18 font.Face
 )
 
 func init() {
@@ -34,8 +36,16 @@ func init() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	mPlus1pRegular, err = opentype.NewFace(tt2, &opentype.FaceOptions{
-		Size:    36,
+	mPlus1pRegular11, err = opentype.NewFace(tt2, &opentype.FaceOptions{
+		Size:    11,
+		DPI:     dpi,
+		Hinting: font.HintingVertical,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	mPlus1pRegular18, err = opentype.NewFace(tt2, &opentype.FaceOptions{
+		Size:    18,
 		DPI:     dpi,
 		Hinting: font.HintingVertical,
 	})
@@ -54,7 +64,28 @@ func (g *game) Update() error {
 
 func (g *game) Draw(screen *ebiten.Image) {
 	screen.Fill(color.Gray{Y: 239})
-	//ebitenutil.DrawRect(screen, boardStartX, boardStartY, boardWidthPx, boardWidthPx, color.Gray{Y: 200})
-	text.Draw(screen, "SERG", pressStart2P, 50, 80, color.Black)
-	text.Draw(screen, "SERG", mPlus1pRegular, 50, 130, color.Black)
+	text.Draw(screen, "SERG", pressStart2P, 40, 60, color.Gray{Y: 80})
+	text.Draw(screen, "bsski 2023", mPlus1pRegular11, 82, 75, color.Gray{Y: 200})
+
+	squareSize := 10
+	y := 19
+	x := 222
+
+	ebitenutil.DrawRect(screen, float64(x), float64(y), float64(42*squareSize-1), float64(42*squareSize-1), color.Gray{Y: 210})
+
+	for i := 1; i < 42; i++ {
+		ebitenutil.DrawLine(screen, float64(x), float64(y+i*squareSize), float64(x+42*squareSize-1), float64(y+i*squareSize), color.Gray{Y: 239})
+	}
+	for j := 1; j < 42; j++ {
+		ebitenutil.DrawLine(screen, float64(x+j*squareSize), float64(y), float64(x+j*squareSize), float64(y+42*squareSize-1), color.Gray{Y: 239})
+	}
+
+	ebitenutil.DrawRect(screen, 29, 124, 162, 300, color.White)
+
+	ebitenutil.DrawRect(screen, 860, 44, 176, 6, color.Gray{Y: 210})
+	ebitenutil.DrawRect(screen, 850, 50, 196, 609, color.Gray{Y: 210})
+	ebitenutil.DrawRect(screen, 37, 459, 801, 191, color.White)
+
+	//ebitenutil.DrawLine(screen, float64(x+j*squareSize), float64(y), float64(x+j*squareSize), float64(y+42*squareSize-1), color.Gray{Y: 210})
+
 }
